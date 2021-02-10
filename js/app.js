3e6fda1ee5886bc61d990f5e642f3b632f390336
Monica <monica.rolle13@gmail.com>
2021-02-10 13:22:10 +0100

    css final, create object and final html

---
+++ js/app.js
@@ -0,0 +1,7 @@
+var hotel= {
+    name: "Rui Plaza España",
+    location:"Calle Gran Vía, 84, Centro de Madrid, 28013 Madrid, España",
+    img:"https://z.cdrst.com/foto/hotel-sf/c0380/granderesp/riu-plaza-espana-exterior-a69344a.jpg",
+};
+
+
